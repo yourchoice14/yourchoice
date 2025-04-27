9677006e1e425d433e28fa11a8560982db44caa0
yourchoice14 <yourchoice.david.swissa@gmail.com>
2025-04-27 03:22:20 +0300

    Create navbar.js

---
+++ assets/js/navbar.js
@@ -0,0 +1,12 @@
+// navbar.js
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
